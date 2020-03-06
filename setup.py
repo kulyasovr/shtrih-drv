aa19136122bd146d4d8736a07e2054d61be78cfa
kulyasovr <kulyasovrom@gmail.com>
2020-03-06 13:51:51 +0300

    setup.py init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='shtrih-fr-driver',
+    version='0.1.2',
+    packages=[''],
+    url='https://github.com/kulyasovr/shtrih-fr-driver',
+    license='MIT',
+    author='Roman Kulyasov',
+    author_email='',
+    description=''
+)
