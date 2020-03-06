cfa55dbb5e3fe6dd5fb385088054005ebfc541da
kulyasovr <kulyasovrom@gmail.com>
2020-03-06 15:37:39 +0300

    Modified setup.py and __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shtrih-drv',
-    version='0.1.5',
+    version='0.1.2',
     packages=['shtrih_drv'],
     url='https://github.com/kulyasovr/shtrih-fr-driver',
     license='MIT',
